80d25827a0415f9377f0c3a77921eeb20535e612
Shailesh singh <singhshailesh2901@gmail.com>
2023-09-24 20:52:10 +0530

    ok

---
+++ learn.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the first number:"<<endl;
+    int n1;
+    cin>>n1;
+    cout<<"Enter second number:"<<endl;
+    int n2;
+    cin>>n2;
+   int  add=n1+n2;
+    cout<<add<<endl;
+    return 0;
+}
