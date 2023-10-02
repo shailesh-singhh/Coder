54ecec6e7d2692b7bc9f292efaab26ad4154ffe4
Shailesh singh <singhshailesh2901@gmail.com>
2023-10-02 12:12:29 +0530

    code to print nth fibonacci  number

---
+++ recursion_fibonocci.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int fib(int n){
+    if(n==0  or n==1)
+    return n;
+    return fib(n-1)+fib(n-2);
+}
+int main(){
+    int result = fib(8);
+    cout<<"fibonoci number is : "<<result<<endl;
+    return 0;
+    }
