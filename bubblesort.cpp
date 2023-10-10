ec64abb65549ad1db49167bfacd17f739cb8e42f
Shailesh singh <singhshailesh2901@gmail.com>
2023-10-10 19:08:45 +0530

    Correction made

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -5,7 +5,7 @@ void bubbleSort(vector<int> &v){
     int n = v.size();
     for(int i=0 ;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
-            if(v[j]>v[j-1]){
+            if(v[j]>v[j+1]){
                 swap(v[j],v[j+1]);
             }
         }
