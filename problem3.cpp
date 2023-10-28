af498b5d86f39ab69ad545896f230600e068f9e2
Shailesh singh <singhshailesh2901@gmail.com>
2023-10-28 23:53:36 +0530

    making funtion

---
+++ problem3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int sum(int a , int b ){
+   int add = a + b;
+   return add;}
+int subtraction(int a, int b){
+ int sub = a-b;
+ return sub;
+}
+int main(){
+   
+cout<<sum(3,4)<<endl;
+cout<<subtraction(4,3)<<endl;
+    return 0;
+}
