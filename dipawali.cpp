66be55b5b9b8f34cf0353601beb62f85b6f6133f
Shailesh singh <singhshailesh2901@gmail.com>
2023-11-21 17:57:03 +0530

    on dipawali

---
+++ dipawali.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    cout<<"Enter the date :"<<endl;
+    cin>>i;
+    if(i==12){
+        cout<<"Happy Birthday Nilesh And Happy Dipawali  -- Very Nice day"<<endl;
+    }
+    else{
+        cout<<"Nice day"<<endl;
+    }
+    return 0;
+}
