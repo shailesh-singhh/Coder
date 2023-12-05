6e05034290904bea2c9d697b1f716fa9d77ffede
Shailesh singh <singhshailesh2901@gmail.com>
2023-12-05 15:45:17 +0530

    constructor revision

--- constru.cpp
+++ constru.cpp
@@ -5,16 +5,22 @@ class test{
     int n;
      
      public:
-     test(){
+     test(){                      //-------------------------Default Constructor-----------------------------------//
         cout<<"Constructor is called ...."<<endl;
         n = 0;
+     }
+     test(int num){                   //--------------------------parametrised Constructor------------------------//
+      cout<<"As parameter is passed it is paramaterised constructor ."<<endl;
+      n = num;
      }
      void display(){
         cout<<" n :"<<n<<endl;
      }
 };
 int main(){
-    test t;
+   test t2;
+    test t(20);
+    t2.display();
     t.display();
     return 0;   
 }
