bd49979afa526c82c0be3ca86a381f2708bf508c
Shailesh singh <singhshailesh2901@gmail.com>
2023-12-06 10:26:33 +0530

    example of constructor

--- destructor.cpp
+++ destructor.cpp
@@ -1,1 +1,25 @@
- 
+ #include<iostream>
+ using namespace std;
+ class test{
+    private: 
+    int n;
+    public:
+    test(){
+        cout<<"Constructor is called :"<<endl;
+
+    }
+    ~test(){
+        cout<<"Destructor is called : "<<endl;
+    }
+    
+ };  
+ void objectCreate(){
+    test t1 ,t2;
+ }
+ int main(){
+   objectCreate();
+   cout<<"Back in main "<<endl;
+   system("pause");
+   test t3;
+   return 0;
+ }
