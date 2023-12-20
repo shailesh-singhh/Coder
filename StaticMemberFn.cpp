6a5d143fa27e3648b36ad51ad76519aefaeea451
Shailesh singh <singhshailesh2901@gmail.com>
2023-12-20 19:41:10 +0530

    Static Data Member

---
+++ StaticMemberFn.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Counter{
+    private : 
+    static int count;
+    public:
+     Counter(){
+                count++;
+    }
+    void show(){
+        cout<<"You created  "<<count<<" Objects of the class  "<<endl;
+    }
+};
+    int Counter:: count = 0;
+int main(){
+      
+Counter obj1;
+Counter obj2;
+obj1.show();
+
+}
