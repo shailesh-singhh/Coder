e0798e08d87db148e62e68699b05a8029c89193a
Shailesh singh <singhshailesh2901@gmail.com>
2023-12-30 11:08:54 +0530

    Public Inheritance program

---
+++ PublicInheritance.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class parent{
+    private:
+    
+    protected :
+    int b;
+    int a;
+    public:
+    int c;
+    
+    
+};
+class child : public parent{
+    public:
+    void get(){
+        cout<<"Enter the first value "<<endl;
+        cin>>a;
+        cout<<"Enter the second value "<<endl;
+        cin>>b;
+        cout<<"Enter the third value "<<endl;
+        cin>>c;
+    }
+    void show(){
+        cout<<"The first value is "<<a<<endl;
+        cout<<"The first value is "<<b<<endl;
+        cout<<"The first value is "<<c<<endl;
+    }
+};
+int main(){
+    child obj1;
+    obj1.get();
+    obj1.show();
+    cout<<"Enter value of c in main :"<<endl;
+    cin>>obj1.c;
+   
+}
