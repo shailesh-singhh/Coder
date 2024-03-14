aa839f6f481d36678bc7dc2286ea8ca8d0d97df0
Shailesh singh <singhshailesh2901@gmail.com>
2024-03-14 16:28:52 +0530

    recursiom

---
+++ Rec.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int number( n ){
+    int n;
+    cout<<"Enter the number till you want to print :"<<endl;
+    cin>>n;
+    if(count >= n ){
+        return n;
+    }
+    
+}
